0450aad8f0804434435030acc14c1194d901a975
William Bojczuk <williambojczuk@gmail.com>
2025-06-02 15:28:38 -0400

    updated

--- src/app/(mainsite)/components/applypage/ApplyContent/ApplyContent.tsx
+++ src/app/(mainsite)/components/applypage/ApplyContent/ApplyContent.tsx
@@ -1,13 +1,13 @@
 import styles from './applycontent.module.css';
+// @ts-ignore
+import JotformEmbed from "react-jotform-embed"
 
 export default function ApplyContent(){
 return (
  <div className={styles.applyContent}>
 <div className="center">
     <div className={styles.formWrapper}>
-        <div className={styles.formContent} dangerouslySetInnerHTML={{__html: `<script type="text/javascript" src="https://form.jotform.com/jsform/251526194582158"></script>`}}>
-          
-        </div>
+        <JotformEmbed src="https://form.jotform.com/251526194582158" />
     </div>
 </div>
  </div>
--- src/app/(mainsite)/components/hiringpage/HiringContent/HiringContent.tsx
+++ src/app/(mainsite)/components/hiringpage/HiringContent/HiringContent.tsx
@@ -1,13 +1,12 @@
 import styles from './hiringcontent.module.css';
-
+// @ts-ignore
+import JotformEmbed from "react-jotform-embed"
 export default function HiringContent(){
 return (
  <div className={styles.applyContent}>
 <div className="center">
     <div className={styles.formWrapper}>
-        <div className={styles.formContent} dangerouslySetInnerHTML={{__html: `<script type="text/javascript" src="https://form.jotform.com/jsform/251526182498161"></script>`}}>
-          
-        </div>
+       <JotformEmbed src="https://form.jotform.com/251526182498161"/>
     </div>
 </div>
  </div>
