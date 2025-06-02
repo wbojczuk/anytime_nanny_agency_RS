b81c68cd0c48cdede541c66aef75700016f425e1
William Bojczuk <williambojczuk@gmail.com>
2025-06-02 15:46:19 -0400

    please work

--- src/app/(mainsite)/components/applypage/ApplyContent/ApplyContent.tsx
+++ src/app/(mainsite)/components/applypage/ApplyContent/ApplyContent.tsx
@@ -1,12 +1,12 @@
 import styles from './applycontent.module.css';
 // @ts-ignore
-import JotFormReact from "jotform-react"
 export default function ApplyContent(){
 return (
  <div className={styles.applyContent}>
 <div className="center">
     <div className={styles.formWrapper}>
-          <JotFormReact formURL="https://form.jotform.com/251526194582158" />
+            <iframe style={{width: "100%", minHeight: "100vh", overflowY: "scroll"}} id="JotFormIFrame-251526194582158" title="Nanny Application Form
+" allow="geolocation; microphone; camera; fullscreen; payment" src="https://form.jotform.com/251526194582158" frameBorder="0"scrolling="yes" > </iframe> <script src='https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js'></script> <script>window.jotformEmbedHandler("iframe[id='JotFormIFrame-251526194582158']", "https://form.jotform.com/")</script> 
 
     </div>
 </div>
